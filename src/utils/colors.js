3323940e36f5093b625b0611010d51d859a9d755
Gebreyesus <biniam.gebreyesus@cgi.com>
2017-11-15 09:58:34 +0000

    removed unused variables and refactor

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -4,6 +4,12 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var cyan500 = exports.cyan500 = '#00bcd4';
+var grey300 = exports.grey300 = '#e0e0e0';
+var white = exports.white = '#ffffff';
+var fullBlack = exports.fullBlack = 'rgba(0, 0, 0, 1)';
+var darkBlack = exports.darkBlack = 'rgba(0, 0, 0, 0.87)';
+
 //Added by Aran Bacall 25/09/2017 CGI Colours
 var cgiRed = exports.cgiRed = 'rgba(227,25,55,1)';
 var cgiBeet = exports.cgiBeet = '#991f3d';
@@ -11,7 +17,3 @@ var cgiPumpkin = exports.cgiPumpkin = '#ff6a00';
 var cgiHoney = exports.cgiHoney = '#f2a200';
 var cgiCloud = exports.cgiCloud = '#a5acb0';
 var cgiIce = exports.cgiIce = '#a1c4d0';
-
-
-
-
