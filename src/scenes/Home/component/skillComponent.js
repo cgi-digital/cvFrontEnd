481764c8dc469cd405bb327440b6e68669dcb0e8
Gebreyesus <biniam.gebreyesus@cgi.com>
2017-11-23 10:33:16 +0000

    added skillComponent

---
+++ src/scenes/Home/component/skillComponent.js
@@ -0,0 +1,43 @@
+import  React from 'react';
+class skillComponent extends React.Component {
+
+    componentDidMount(){
+        this.props.skills
+    }
+
+    render() {
+
+        const { skills } = this.props;
+
+        return (
+            <div id="skills" className="tab-pane fade in active">
+            <div className="paper pd15 mb20 tab-header">
+              <h2 className="mb20">Skills</h2>
+            </div>
+            <div className="table-responsive">
+              <table className="table paper">
+                <thead>
+                  <tr>
+                    <th>Skill</th>
+                    <th>Level</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {skills.map((skill, index) => {
+                    return (
+                      <tr>
+                        <td key={index}>{skill.skill}</td>
+                        <td >{skill.level}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        );
+    }
+
+}
+
+export default skillComponent;
